3e3f7902ad007b7dfdca05d6b51bb972efa8e731
Maksim <reckless04@mail.ru>
2016-12-18 01:48:13 +0200

    simple dev & prod mods

---
+++ README.md
@@ -0,0 +1,21 @@
+#GETTING STARTED
+__________________________
+
+1. run `npm i webpack`
+2. install devDependencies
+ 
+#PRODUCTION MODE ( if use linux/mac, use command without `set`)
+__________________________
+```js
+set NODE_ENV=production 
+
+webpack
+```    
+
+#DEVEVLOPMENT MODE 
+__________________________
+```js
+set NODE_ENV=production 
+
+webpack
+```    
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './home',
+  output: {
+    filename: 'build.js',
+    library: 'home'
+  },
+  watch: NODE_ENV === 'development',
+  devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map': null,
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+          presets: ['es2015', 'stage-0']
+      }
+    }]
+  }
+};
+
+if (NODE_ENV === 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true
+      }
+    })
+  )
+}
